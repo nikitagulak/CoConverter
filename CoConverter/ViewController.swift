5f7ae9ee14d483ac7ce2e295f4bbc6fa78499d99
Nikita <myepicland@gmail.com>
2019-05-04 20:47:12 +0200

    Initial commit

---
+++ CoConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoConverter
+//
+//  Created by Nick on 04/05/2019.
+//  Copyright © 2019 Nikita Gulak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
