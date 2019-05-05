c5c4eb568edc65df7eb28cdf7c262f8bfee5e865
Nikita <myepicland@gmail.com>
2019-05-05 03:40:27 +0200

    Built interface, added currencies list.

---
+++ CoConverter/Controller/AddCurrencyController.swift
@@ -0,0 +1,33 @@
+//
+//  AddCurrencyController.swift
+//  CoConverter
+//
+//  Created by Nick on 04/05/2019.
+//  Copyright © 2019 Nikita Gulak. All rights reserved.
+//
+
+import UIKit
+
+class AddCurrencyController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
